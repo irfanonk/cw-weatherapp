ddb170708433150d89ac860913532eed54de9c1c
irfan önk <onk.irfan@gmail.com>
2023-03-29 23:56:08 +0300

    add weather card

---
+++ helper.js
@@ -0,0 +1,50 @@
+function fToC(fahrenheit) {
+  const fTemp = fahrenheit;
+  const fToCel = ((fTemp - 32) * 5) / 9;
+  return fToCel;
+}
+function kToC(kelvin) {
+  const kTemp = kelvin;
+  const kToCel = kelvin - 273.15;
+  return kToCel.toFixed(2);
+}
+
+const weather = {
+  dt: 1680123600,
+  main: {
+    temp: 273.88,
+    feels_like: 273.88,
+    temp_min: 273.76,
+    temp_max: 273.88,
+    pressure: 1023,
+    sea_level: 1023,
+    grnd_level: 970,
+    humidity: 92,
+    temp_kf: 0.12,
+  },
+  weather: [
+    {
+      id: 600,
+      main: "Snow",
+      description: "hafif kar yağışlı",
+      icon: "13n",
+    },
+  ],
+  clouds: {
+    all: 100,
+  },
+  wind: {
+    speed: 1.13,
+    deg: 291,
+    gust: 1.96,
+  },
+  visibility: 4837,
+  pop: 0.59,
+  snow: {
+    "3h": 0.7,
+  },
+  sys: {
+    pod: "n",
+  },
+  dt_txt: "2023-03-29 21:00:00",
+};
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // console.log("axios", axios);
-
+const APIKEY = "854312915ee06dc91df05e3b53609c26";
 let countries = null;
 const selectCountryElm = document.getElementById("select-countries");
 const selectCityElm = document.getElementById("select-cities");
+const weatherCardsElm = document.getElementById("weather-cards");
+console.log("weatherCardsElm:", weatherCardsElm);
 
 const onChangeCountry = (e) => {
   console.log("e", e.target.value);
@@ -26,10 +28,54 @@ const onChangeCountry = (e) => {
 const onChangeCity = async (e) => {
   const selectedCity = e.target.value;
   console.log("selectedCity:", selectedCity);
-  const cityData = await axios.get(
-    `http://geodb-free-service.wirefreethought.com/v1/geo/cities?namePrefix=${selectedCity}&hateoasMode=false&limit=5&offset=0`
-  );
-  console.log("cityData:", cityData.data.data[0]);
+
+  try {
+    const cityData = await axios.get(
+      `http://geodb-free-service.wirefreethought.com/v1/geo/cities?namePrefix=${selectedCity}&hateoasMode=false&limit=5&offset=0`
+    );
+    console.log("cityData:", cityData.data.data[0]);
+    const lat = cityData.data.data[0].latitude;
+    const lon = cityData.data.data[0].longitude;
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKEY}&lang=tr`;
+
+    const cityWeather = await axios.get(weatherUrl);
+    console.log("cityWeather:", cityWeather.data.list);
+
+    cityWeather.data.list.slice(0, 7).forEach((element) => {
+      const weather = element;
+      //   console.log("element:", element);
+      const celcius = kToC(weather.main.temp);
+
+      const description = weather.weather[0].description;
+      const main = weather.weather[0].main;
+
+      const { pressure } = weather.main;
+      const { speed: windSpeed } = weather.wind;
+
+      const cardDiv = `        
+    <div class="card" id="weather-card">
+          <div class="content">
+            <div class="header">${weather.dt_txt}</div>
+            <div class="meta">${celcius}</div>
+            <div class="description">
+              ${main}
+            </div>
+            <div class="description">
+              ${description}
+            </div>
+          </div>
+          <div class="extra content">
+            <div class="ui two buttons">
+              <div class="ui basic green button">Pressure: ${pressure}</div>
+              <div class="ui basic red button">WindSpeed: ${windSpeed} </div>
+            </div>
+          </div>
+        </div>`;
+      weatherCardsElm.innerHTML += cardDiv;
+    });
+  } catch (error) {
+    console.log("error:", error);
+  }
 };
 
 selectCountryElm.addEventListener("change", onChangeCountry);
